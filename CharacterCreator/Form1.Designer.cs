1adb472af553f9a8f0656ab2d414525918c64134
SennahW <johannes.wetterlund@gmail.com>
2019-04-02 10:53:33 +0200

    Added some basic UI

--- CharacterCreator/Form1.Designer.cs
+++ CharacterCreator/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CharacterCreator
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -29,12 +29,92 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtBoxName = new System.Windows.Forms.TextBox();
+            this.grpBoxGender = new System.Windows.Forms.GroupBox();
+            this.rdoBtnMale = new System.Windows.Forms.RadioButton();
+            this.rdoBtnFemale = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.grpBoxGender.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(30, 28);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(61, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Enter name";
+            // 
+            // txtBoxName
+            // 
+            this.txtBoxName.Location = new System.Drawing.Point(108, 28);
+            this.txtBoxName.Name = "txtBoxName";
+            this.txtBoxName.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxName.TabIndex = 1;
+            this.txtBoxName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // grpBoxGender
+            // 
+            this.grpBoxGender.Controls.Add(this.rdoBtnFemale);
+            this.grpBoxGender.Controls.Add(this.rdoBtnMale);
+            this.grpBoxGender.Location = new System.Drawing.Point(33, 76);
+            this.grpBoxGender.Name = "grpBoxGender";
+            this.grpBoxGender.Size = new System.Drawing.Size(200, 100);
+            this.grpBoxGender.TabIndex = 2;
+            this.grpBoxGender.TabStop = false;
+            this.grpBoxGender.Text = "Gender";
+            // 
+            // rdoBtnMale
+            // 
+            this.rdoBtnMale.AutoSize = true;
+            this.rdoBtnMale.Location = new System.Drawing.Point(7, 46);
+            this.rdoBtnMale.Name = "rdoBtnMale";
+            this.rdoBtnMale.Size = new System.Drawing.Size(48, 17);
+            this.rdoBtnMale.TabIndex = 0;
+            this.rdoBtnMale.TabStop = true;
+            this.rdoBtnMale.Text = "Male";
+            this.rdoBtnMale.UseVisualStyleBackColor = true;
+            // 
+            // rdoBtnFemale
+            // 
+            this.rdoBtnFemale.AutoSize = true;
+            this.rdoBtnFemale.Location = new System.Drawing.Point(99, 46);
+            this.rdoBtnFemale.Name = "rdoBtnFemale";
+            this.rdoBtnFemale.Size = new System.Drawing.Size(59, 17);
+            this.rdoBtnFemale.TabIndex = 1;
+            this.rdoBtnFemale.TabStop = true;
+            this.rdoBtnFemale.Text = "Female";
+            this.rdoBtnFemale.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.grpBoxGender);
+            this.Controls.Add(this.txtBoxName);
+            this.Controls.Add(this.lblName);
+            this.Name = "frmMain";
+            this.Text = "Character Creator";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.grpBoxGender.ResumeLayout(false);
+            this.grpBoxGender.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtBoxName;
+        private System.Windows.Forms.GroupBox grpBoxGender;
+        private System.Windows.Forms.RadioButton rdoBtnFemale;
+        private System.Windows.Forms.RadioButton rdoBtnMale;
     }
 }
 
--- CharacterCreator/Form1.cs
+++ CharacterCreator/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace CharacterCreator
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
