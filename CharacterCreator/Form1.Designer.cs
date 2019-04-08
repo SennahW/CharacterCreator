f08f815ec7661c4f624ba396db81f27fff828cc7
SennahW <johannes.wetterlund@gmail.com>
2019-04-08 13:06:14 +0200

    Fixed thingys

--- CharacterCreator/Form1.Designer.cs
+++ CharacterCreator/Form1.Designer.cs
@@ -33,8 +33,20 @@
             this.lblName = new System.Windows.Forms.Label();
             this.txtBoxName = new System.Windows.Forms.TextBox();
             this.grpBoxGender = new System.Windows.Forms.GroupBox();
-            this.rdoBtnMale = new System.Windows.Forms.RadioButton();
             this.rdoBtnFemale = new System.Windows.Forms.RadioButton();
+            this.rdoBtnMale = new System.Windows.Forms.RadioButton();
+            this.btnPowerDown = new System.Windows.Forms.Button();
+            this.btnPowerUp = new System.Windows.Forms.Button();
+            this.btnAgilityDown = new System.Windows.Forms.Button();
+            this.btnAgilityUp = new System.Windows.Forms.Button();
+            this.btnInteligensDown = new System.Windows.Forms.Button();
+            this.btnInteligensUp = new System.Windows.Forms.Button();
+            this.lblCharacteristicsPoints = new System.Windows.Forms.Label();
+            this.lblPower = new System.Windows.Forms.Label();
+            this.lblAgility = new System.Windows.Forms.Label();
+            this.lblInteligens = new System.Windows.Forms.Label();
+            this.btnSaveFile = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.grpBoxGender.SuspendLayout();
             this.SuspendLayout();
@@ -42,61 +54,196 @@
             // lblName
             // 
             this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(30, 28);
+            this.lblName.Location = new System.Drawing.Point(40, 34);
+            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(61, 13);
+            this.lblName.Size = new System.Drawing.Size(81, 17);
             this.lblName.TabIndex = 0;
             this.lblName.Text = "Enter name";
             // 
             // txtBoxName
             // 
-            this.txtBoxName.Location = new System.Drawing.Point(108, 28);
+            this.txtBoxName.Location = new System.Drawing.Point(144, 34);
+            this.txtBoxName.Margin = new System.Windows.Forms.Padding(4);
             this.txtBoxName.Name = "txtBoxName";
-            this.txtBoxName.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxName.Size = new System.Drawing.Size(132, 22);
             this.txtBoxName.TabIndex = 1;
-            this.txtBoxName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtBoxName.Text = "9";
             // 
             // grpBoxGender
             // 
             this.grpBoxGender.Controls.Add(this.rdoBtnFemale);
             this.grpBoxGender.Controls.Add(this.rdoBtnMale);
-            this.grpBoxGender.Location = new System.Drawing.Point(33, 76);
+            this.grpBoxGender.Location = new System.Drawing.Point(44, 94);
+            this.grpBoxGender.Margin = new System.Windows.Forms.Padding(4);
             this.grpBoxGender.Name = "grpBoxGender";
-            this.grpBoxGender.Size = new System.Drawing.Size(200, 100);
+            this.grpBoxGender.Padding = new System.Windows.Forms.Padding(4);
+            this.grpBoxGender.Size = new System.Drawing.Size(267, 123);
             this.grpBoxGender.TabIndex = 2;
             this.grpBoxGender.TabStop = false;
             this.grpBoxGender.Text = "Gender";
             // 
-            // rdoBtnMale
-            // 
-            this.rdoBtnMale.AutoSize = true;
-            this.rdoBtnMale.Location = new System.Drawing.Point(7, 46);
-            this.rdoBtnMale.Name = "rdoBtnMale";
-            this.rdoBtnMale.Size = new System.Drawing.Size(48, 17);
-            this.rdoBtnMale.TabIndex = 0;
-            this.rdoBtnMale.TabStop = true;
-            this.rdoBtnMale.Text = "Male";
-            this.rdoBtnMale.UseVisualStyleBackColor = true;
-            // 
             // rdoBtnFemale
             // 
             this.rdoBtnFemale.AutoSize = true;
-            this.rdoBtnFemale.Location = new System.Drawing.Point(99, 46);
+            this.rdoBtnFemale.Location = new System.Drawing.Point(132, 57);
+            this.rdoBtnFemale.Margin = new System.Windows.Forms.Padding(4);
             this.rdoBtnFemale.Name = "rdoBtnFemale";
-            this.rdoBtnFemale.Size = new System.Drawing.Size(59, 17);
+            this.rdoBtnFemale.Size = new System.Drawing.Size(75, 21);
             this.rdoBtnFemale.TabIndex = 1;
             this.rdoBtnFemale.TabStop = true;
             this.rdoBtnFemale.Text = "Female";
             this.rdoBtnFemale.UseVisualStyleBackColor = true;
             // 
+            // rdoBtnMale
+            // 
+            this.rdoBtnMale.AutoSize = true;
+            this.rdoBtnMale.Location = new System.Drawing.Point(9, 57);
+            this.rdoBtnMale.Margin = new System.Windows.Forms.Padding(4);
+            this.rdoBtnMale.Name = "rdoBtnMale";
+            this.rdoBtnMale.Size = new System.Drawing.Size(59, 21);
+            this.rdoBtnMale.TabIndex = 0;
+            this.rdoBtnMale.TabStop = true;
+            this.rdoBtnMale.Text = "Male";
+            this.rdoBtnMale.UseVisualStyleBackColor = true;
+            // 
+            // btnPowerDown
+            // 
+            this.btnPowerDown.Location = new System.Drawing.Point(640, 126);
+            this.btnPowerDown.Name = "btnPowerDown";
+            this.btnPowerDown.Size = new System.Drawing.Size(75, 23);
+            this.btnPowerDown.TabIndex = 3;
+            this.btnPowerDown.Text = "-";
+            this.btnPowerDown.UseVisualStyleBackColor = true;
+            this.btnPowerDown.Click += new System.EventHandler(this.btnPowerDown_Click);
+            // 
+            // btnPowerUp
+            // 
+            this.btnPowerUp.Location = new System.Drawing.Point(925, 126);
+            this.btnPowerUp.Name = "btnPowerUp";
+            this.btnPowerUp.Size = new System.Drawing.Size(75, 23);
+            this.btnPowerUp.TabIndex = 4;
+            this.btnPowerUp.Text = "+";
+            this.btnPowerUp.UseVisualStyleBackColor = true;
+            this.btnPowerUp.Click += new System.EventHandler(this.btnPowerUp_Click);
+            // 
+            // btnAgilityDown
+            // 
+            this.btnAgilityDown.Location = new System.Drawing.Point(640, 193);
+            this.btnAgilityDown.Name = "btnAgilityDown";
+            this.btnAgilityDown.Size = new System.Drawing.Size(75, 23);
+            this.btnAgilityDown.TabIndex = 5;
+            this.btnAgilityDown.Text = "-";
+            this.btnAgilityDown.UseVisualStyleBackColor = true;
+            this.btnAgilityDown.Click += new System.EventHandler(this.btnAgilityDown_Click);
+            // 
+            // btnAgilityUp
+            // 
+            this.btnAgilityUp.Location = new System.Drawing.Point(925, 193);
+            this.btnAgilityUp.Name = "btnAgilityUp";
+            this.btnAgilityUp.Size = new System.Drawing.Size(75, 23);
+            this.btnAgilityUp.TabIndex = 6;
+            this.btnAgilityUp.Text = "+";
+            this.btnAgilityUp.UseVisualStyleBackColor = true;
+            this.btnAgilityUp.Click += new System.EventHandler(this.btnAgilityUp_Click);
+            // 
+            // btnInteligensDown
+            // 
+            this.btnInteligensDown.Location = new System.Drawing.Point(640, 259);
+            this.btnInteligensDown.Name = "btnInteligensDown";
+            this.btnInteligensDown.Size = new System.Drawing.Size(75, 23);
+            this.btnInteligensDown.TabIndex = 7;
+            this.btnInteligensDown.Text = "-";
+            this.btnInteligensDown.UseVisualStyleBackColor = true;
+            this.btnInteligensDown.Click += new System.EventHandler(this.btnInteligensDown_Click);
+            // 
+            // btnInteligensUp
+            // 
+            this.btnInteligensUp.Location = new System.Drawing.Point(925, 259);
+            this.btnInteligensUp.Name = "btnInteligensUp";
+            this.btnInteligensUp.Size = new System.Drawing.Size(75, 23);
+            this.btnInteligensUp.TabIndex = 8;
+            this.btnInteligensUp.Text = "+";
+            this.btnInteligensUp.UseVisualStyleBackColor = true;
+            this.btnInteligensUp.Click += new System.EventHandler(this.btnInteligensUp_Click);
+            // 
+            // lblCharacteristicsPoints
+            // 
+            this.lblCharacteristicsPoints.AutoSize = true;
+            this.lblCharacteristicsPoints.Location = new System.Drawing.Point(821, 34);
+            this.lblCharacteristicsPoints.Name = "lblCharacteristicsPoints";
+            this.lblCharacteristicsPoints.Size = new System.Drawing.Size(106, 17);
+            this.lblCharacteristicsPoints.TabIndex = 9;
+            this.lblCharacteristicsPoints.Text = "Points to spend";
+            // 
+            // lblPower
+            // 
+            this.lblPower.AutoSize = true;
+            this.lblPower.Location = new System.Drawing.Point(793, 126);
+            this.lblPower.Name = "lblPower";
+            this.lblPower.Size = new System.Drawing.Size(47, 17);
+            this.lblPower.TabIndex = 10;
+            this.lblPower.Text = "Power";
+            // 
+            // lblAgility
+            // 
+            this.lblAgility.AutoSize = true;
+            this.lblAgility.Location = new System.Drawing.Point(796, 193);
+            this.lblAgility.Name = "lblAgility";
+            this.lblAgility.Size = new System.Drawing.Size(45, 17);
+            this.lblAgility.TabIndex = 11;
+            this.lblAgility.Text = "Agility";
+            // 
+            // lblInteligens
+            // 
+            this.lblInteligens.AutoSize = true;
+            this.lblInteligens.Location = new System.Drawing.Point(796, 264);
+            this.lblInteligens.Name = "lblInteligens";
+            this.lblInteligens.Size = new System.Drawing.Size(68, 17);
+            this.lblInteligens.TabIndex = 12;
+            this.lblInteligens.Text = "Inteligens";
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.Location = new System.Drawing.Point(925, 374);
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(116, 23);
+            this.btnSaveFile.TabIndex = 13;
+            this.btnSaveFile.Text = "Save file";
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.btnSaveFile_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(765, 374);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(116, 23);
+            this.btnOpenFile.TabIndex = 14;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.btnSaveFile);
+            this.Controls.Add(this.lblInteligens);
+            this.Controls.Add(this.lblAgility);
+            this.Controls.Add(this.lblPower);
+            this.Controls.Add(this.lblCharacteristicsPoints);
+            this.Controls.Add(this.btnInteligensUp);
+            this.Controls.Add(this.btnInteligensDown);
+            this.Controls.Add(this.btnAgilityUp);
+            this.Controls.Add(this.btnAgilityDown);
+            this.Controls.Add(this.btnPowerUp);
+            this.Controls.Add(this.btnPowerDown);
             this.Controls.Add(this.grpBoxGender);
             this.Controls.Add(this.txtBoxName);
             this.Controls.Add(this.lblName);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmMain";
             this.Text = "Character Creator";
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
@@ -115,6 +262,18 @@
         private System.Windows.Forms.GroupBox grpBoxGender;
         private System.Windows.Forms.RadioButton rdoBtnFemale;
         private System.Windows.Forms.RadioButton rdoBtnMale;
+        private System.Windows.Forms.Button btnPowerDown;
+        private System.Windows.Forms.Button btnPowerUp;
+        private System.Windows.Forms.Button btnAgilityDown;
+        private System.Windows.Forms.Button btnAgilityUp;
+        private System.Windows.Forms.Button btnInteligensDown;
+        private System.Windows.Forms.Button btnInteligensUp;
+        private System.Windows.Forms.Label lblCharacteristicsPoints;
+        private System.Windows.Forms.Label lblPower;
+        private System.Windows.Forms.Label lblAgility;
+        private System.Windows.Forms.Label lblInteligens;
+        private System.Windows.Forms.Button btnSaveFile;
+        private System.Windows.Forms.Button btnOpenFile;
     }
 }
 
--- CharacterCreator/Form1.cs
+++ CharacterCreator/Form1.cs
@@ -12,12 +12,107 @@ namespace CharacterCreator
 {
     public partial class frmMain : Form
     {
+        float CharateristisPoints = 40;
+        float PowerPoints = 0;
+        float InteligensPoints = 0;
+        float AgilityPoints = 0;
+        
+
         public frmMain()
         {
             InitializeComponent();
+            UpdateValues();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void btnPowerDown_Click(object sender, EventArgs e)
+        {
+            if (PowerPoints > 0)
+            {
+                PowerPoints--;
+                CharateristisPoints++;
+                UpdateValues();
+            }
+        }
+
+        private void btnPowerUp_Click(object sender, EventArgs e)
+        {
+            if (CharateristisPoints > 0)
+            {
+                PowerPoints++;
+                CharateristisPoints--;
+                UpdateValues();
+            }
+        }
+
+        private void btnAgilityUp_Click(object sender, EventArgs e)
+        {
+            if (CharateristisPoints > 0)
+            {
+                AgilityPoints++;
+                CharateristisPoints--;
+                UpdateValues();
+            }
+        }
+
+        private void btnAgilityDown_Click(object sender, EventArgs e)
+        {
+            if (AgilityPoints > 0)
+            {
+                AgilityPoints--;
+                CharateristisPoints++;
+                UpdateValues();
+            }
+        }
+
+        private void btnInteligensDown_Click(object sender, EventArgs e)
+        {
+            if (InteligensPoints > 0)
+            {
+                InteligensPoints--;
+                CharateristisPoints++;
+                UpdateValues();
+            }
+        }
+
+        private void btnInteligensUp_Click(object sender, EventArgs e)
+        {
+            if (CharateristisPoints > 0)
+            {
+                InteligensPoints++;
+                CharateristisPoints--;
+                UpdateValues();
+            }
+        }
+
+        private void UpdateValues()
+        {
+            lblPower.Text = "Power: " + PowerPoints.ToString();
+            lblAgility.Text = "Agility: " + AgilityPoints.ToString();
+            lblInteligens.Text = "Inteligens: " + InteligensPoints.ToString();
+            lblCharacteristicsPoints.Text = "Points to spend: " + CharateristisPoints.ToString();
+        }
+
+        private void btnSaveFile_Click(object sender, EventArgs e)
+        {
+           
+        }
+        /*
+        // Öppna open file dialog så att användaren för välja en fil att läsa ifrån
+        openFileDialog1.ShowDialog();
+        // Skriv innehållet från den valda filen till en textbox
+        textBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+        Exemplet ovan visar hur man med hjälp av en openFileDialog kan få användaren att öppna
+        en fil (exempelvis textfil) och sedan skriva innehållet av den in i en textbox.
+        // Öppna save file dialog så att användaren för välja en fil att spara till
+        saveFileDialog1.ShowDialog();
+        // Skriv innehållet från en textbox till den valda filen
+        File.WriteAllText(saveFileDialog1.FileName, textBox1.Text);
+        I detta exempel får användaren välja en fil via en saveFileDialog som en text ska sparas till.
+        Därefter sparas innehållet i en textbox till denna fil. Observera att eventuellt innehåll som
+        fanns i filen tidigare kommer att skrivas över.
+        */
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
         {
 
         }
